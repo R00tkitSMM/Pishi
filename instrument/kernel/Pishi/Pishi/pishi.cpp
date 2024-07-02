55a0bce2f62912bbe6e291871cd0e9abcdd2cf67
R00tkitSMM <78787782+R00tkitSMM@users.noreply.github.com>
2024-07-02 21:39:20 +0200

    clean up

--- instrument/kernel/Pishi/Pishi/pishi.cpp
+++ instrument/kernel/Pishi/Pishi/pishi.cpp
@@ -29,21 +29,22 @@
 // fake entitlement checkers
 class IOFuzzClient {
 public:
-    OSObject * copyClientEntitlement(task_t task, const char *entitlement);
-    OSObject * AMFIcopyClientEntitlement(task_t task, const char *entitlement);
+    OSObject * copyClientEntitlement(task_t task, const char* entitlement);
+    OSObject * AMFIcopyClientEntitlement(task_t task, const char* entitlement);
 };
 
-OSObject * IOFuzzClient::copyClientEntitlement( task_t task,const char * entitlement ) 
+OSObject * IOFuzzClient::copyClientEntitlement( task_t task,const char* entitlement )
 {
     return kOSBooleanTrue;
 }
 
-OSObject * IOFuzzClient::AMFIcopyClientEntitlement( task_t task,const char * entitlement ) 
+OSObject * IOFuzzClient::AMFIcopyClientEntitlement( task_t task,const char* entitlement )
 {
     return kOSBooleanTrue;
 }
 
 extern "C" {
+
 #define PISHI_IOCTL_MAP          _IOWR('K', 8, struct pishi_buf_desc)
 #define PISHI_IOCTL_START        _IOW('K', 10, uintptr_t)
 #define PISHI_IOCTL_STOP         _IOW('K', 20, uint64_t)
@@ -54,17 +55,17 @@ extern "C" {
 #define PISHI_DEVNODE "pishi"
 #define PISHNI_PATH "/dev/" PISHI_DEVNODE
 
-kern_return_t helper_start(kmod_info_t * ki, void *d);
-kern_return_t helper_stop(kmod_info_t *ki, void *d);
+kern_return_t helper_start(kmod_info_t* ki, void *d);
+kern_return_t helper_stop(kmod_info_t* ki, void *d);
 static int pishi_ioctl(dev_t dev, unsigned long cmd, caddr_t _data, int fflag, proc_t p);
 static int pishi_open(dev_t dev, int flags, int devtype, proc_t p);
 static int pishi_close(dev_t dev, int flags, int devtype, proc_t p);
 void sanitizer_cov_trace_pc(uint64_t address);
 void fuzz(char*  buffer);
 
-uintptr_t* buffer_instrument = NULL;
-IOMemoryMap *currnet_task_map = NULL;
-IOBufferMemoryDescriptor *memoryDescriptor = NULL;
+uintptr_t* instrument_buffer = NULL;
+IOMemoryMap* currnet_task_map = NULL;
+IOBufferMemoryDescriptor* memoryDescriptor = NULL;
 
 bool do_instrument = false;
 volatile bool do_log = false;
@@ -120,6 +121,7 @@ uintptr_t* map_memoryinto_current_task()
 {
     memoryDescriptor = createSharedMemory( sizeof(kcov) + (0x20000 * (sizeof(uintptr_t)) ));
     if ( !memoryDescriptor ) {
+
         my_printf("[meysam] Failed to create memory descriptor\n");
         return NULL;
     }
@@ -127,13 +129,14 @@ uintptr_t* map_memoryinto_current_task()
     currnet_task_map = memoryDescriptor->createMappingInTask(current_task(), 0, kIOMapAnywhere);
     
     if ( !currnet_task_map ) {
+
         my_printf("[meysam] Failed to map memory descriptor\n");
         memoryDescriptor->release();
         memoryDescriptor = NULL;
         return NULL;
     }
     
-    buffer_instrument = (uintptr_t*) memoryDescriptor->getBytesNoCopy();
+    instrument_buffer = (uintptr_t*) memoryDescriptor->getBytesNoCopy();
     return (uintptr_t*) currnet_task_map->getVirtualAddress();
 }
 
@@ -143,6 +146,7 @@ static int
 pishi_open(dev_t dev, int flags, int devtype, proc_t p)
 {
     if ( isDeviceOpen ) {
+
         return -EBUSY;
     }
     
@@ -155,14 +159,16 @@ pishi_close(dev_t dev, int flags, int devtype, proc_t p)
 {
     do_instrument = false;
     instrumented_thread = UINT_MAX;
-    buffer_instrument = NULL;
+    instrument_buffer = NULL;
     
     if ( memoryDescriptor ) {
+
         memoryDescriptor->release();
         memoryDescriptor = NULL;
     }
     
     if( currnet_task_map ) {
+
         currnet_task_map->release();
         currnet_task_map = NULL;
     }
@@ -175,15 +181,20 @@ static int
 pishi_ioctl(dev_t dev, unsigned long cmd, caddr_t _data, int fflag, proc_t p)
 {
     switch (cmd) {
+
         case PISHI_IOCTL_MAP: {
-            if( buffer_instrument ) {
-                my_printf("[meysam] PISHI_IOCTL_MAP IOCTLTL buffer_instrument is already mapped\n");
+            
+            my_printf("[meysam] PISHI_IOCTL_MAP instrumented_thread %llu do_instrument %d" ,instrumented_thread, do_instrument);
+            if( instrument_buffer ) {
+
+                my_printf("[meysam] PISHI_IOCTL_MAP IOCTLTL instrument_buffer is already mapped\n");
                 break;
             }
             
             uintptr_t* maped_address =  map_memoryinto_current_task();
             if ( !maped_address ) {
-                my_printf("[meysam] PISHI_IOCTL_MAP IOCTLTL maped_address is NULL\n");
+
+                my_printf("[meysam] PISHI_IOCTL_MAP IOCTL maped_address is NULL\n");
                 break;
             }
             
@@ -193,70 +204,72 @@ pishi_ioctl(dev_t dev, unsigned long cmd, caddr_t _data, int fflag, proc_t p)
             
             do_instrument = false;
             instrumented_thread = UINT_MAX;
-            kcov* area = (kcov*) buffer_instrument;
+            kcov* area = (kcov*) instrument_buffer;
             area->kcov_pos = 0;
             
-            my_printf("[meysam:PISHI_IOCTL_MAP] instrumented_thread %llu, do_instrument %d" ,instrumented_thread, do_instrument);
             break;
         }
             
         case PISHI_IOCTL_START: {
+            
+            my_printf("[meysam] PISHI_IOCTL_START ThreadID %llu do_instrument %d\n", instrumented_thread, do_instrument);
+            if (!instrument_buffer) {
 
-            if (!buffer_instrument) {
-                my_printf("[meysm] PISHI_IOCTL_START buffer_instrument is NULL\n");
+                my_printf("[meysam] PISHI_IOCTL_START instrument_buffer is NULL\n");
                 break;
             }
             if ( do_instrument ) {
-                my_printf("[meysm] PISHI_IOCTL_START do_instrument is already on\n");
+
+                my_printf("[meysam] PISHI_IOCTL_START do_instrument is already on\n");
                 break;
             }
             
-            uint64_t threadID = thread_tid(current_thread());
-            instrumented_thread =  threadID;
-            do_instrument = true;
-            
-            kcov* area = (kcov*) buffer_instrument;
+            kcov* area = (kcov*) instrument_buffer;
             if( area )
                 area->kcov_pos = 0;
-            
-            my_printf("[meysam:PISHI_IOCTL_START] ThreadID %llu, do_instrument %d\n" ,instrumented_thread, do_instrument);
-            break;
-        }
-        case PISHI_IOCTL_STOP: {
-            do_instrument = false;
-            instrumented_thread = UINT_MAX;
-            my_printf("[meysam:PISI_IOCTL_STOP] ThreadID %llu, do_instrument %d\n" ,instrumented_thread, do_instrument);
-            break;
-        }
-        case PISHI_IOCTL_TEST: {
-            // generate hit, to test full package, otherwise we have to wait until ghidra finishes its job.
-            sanitizer_cov_trace_pc(0x4141414141);
-            my_printf("[meysam:PISI_IOCTL_TEST] ThreadID %llu, do_instrument %d\n" ,instrumented_thread, do_instrument);
-            break;
-        }
-        case PISHI_IOCTL_FUZZ: {
-            fuzz((char*)_data);
+
+            instrumented_thread = thread_tid(current_thread());
+            do_instrument = true;
+
             break;
         }
         case PISHI_IOCTL_UNMAP: {
-            my_printf("[meysam:PISI_IOCTL_UNMAP] ThreadID %llu, do_instrument %d\n" ,instrumented_thread, do_instrument);
-            
+
+            my_printf("[meysam] PISI_IOCTL_UNMAP ThreadID %llu do_instrument %d\n", instrumented_thread, do_instrument);
             do_instrument = false;
             instrumented_thread = UINT_MAX;
-            buffer_instrument = NULL;
-            
-            if ( do_instrument )
-                break;
-            
+            instrument_buffer = NULL;
+
             if ( memoryDescriptor ) {
+
                 memoryDescriptor->release();
                 memoryDescriptor = NULL;
             }
             
             if( currnet_task_map ) {
+
                 currnet_task_map->release();
                 currnet_task_map = NULL;
             }
+
+            break;
+        }
+        case PISHI_IOCTL_STOP: {
+
+            do_instrument = false;
+            instrumented_thread = UINT_MAX;
+            my_printf("[meysam] PISI_IOCTL_STOP ThreadID %llu do_instrument %d\n", instrumented_thread, do_instrument);
+            break;
+        }
+        case PISHI_IOCTL_TEST: {
+
+            my_printf("[meysam] PISI_IOCTL_TEST ThreadID %llu do_instrument %d\n", instrumented_thread, do_instrument);
+            sanitizer_cov_trace_pc(0x4141414141);
+            break;
+        }
+        case PISHI_IOCTL_FUZZ: {
+
+            fuzz((char*)_data);
             break;
         }
     }
@@ -272,15 +285,18 @@ ksancov_dev_clone(dev_t dev, int action)
 void sanitizer_cov_trace_pc(uint64_t address)
 {
     if (__improbable(do_instrument)) {
-        my_printf("[meysam:after do_instrument] instrumented_thread %llu, do_instrument %d\n" ,instrumented_thread, do_instrument);
+
+        my_printf("[meysam] sanitizer_cov_trace_pc instrumented_thread %llu do_instrument %d\n", instrumented_thread, do_instrument);
         
-        if (__improbable(buffer_instrument == NULL))
+        if (__improbable(instrument_buffer == NULL))
             return;
         
         if(__improbable(instrumented_thread == thread_tid(current_thread()))) {
-            kcov* area = (kcov*) buffer_instrument;
+
+            kcov* area = (kcov*) instrument_buffer;
             /* The first 64-bit word is the number of subsequent PCs. */
             if (__probable(area->kcov_pos < 0x20000)) {
+
                 unsigned long pos = area->kcov_pos;
                 area->kcov_area[pos] = address;
                 area->kcov_pos +=1;
@@ -348,6 +364,7 @@ void thread_create_dev_callback(void *, wait_result_t)
     IOSleep(1000);
     dev_major = cdevsw_add(-1, &pishi_cdev);
     if (dev_major < 0) {
+
         my_printf("meysam: failed to allocate major device node\n");
         return ;
     }
@@ -355,6 +372,7 @@ void thread_create_dev_callback(void *, wait_result_t)
     void *node = devfs_make_node_clone(dev, DEVFS_CHAR, UID_ROOT, GID_WHEEL, 0666,
                                        ksancov_dev_clone, PISHI_DEVNODE);
     if (!node) {
+
         my_printf("meysam: failed to create device node\n");
     }
     
@@ -374,6 +392,5 @@ kern_return_t Pishi_start(kmod_info_t * ki, void *d)
     create_dev_thread(thread_create_dev_callback);
     return KERN_SUCCESS;
 }
-
 
 }
